500e81b701dde48fa28afaf2e7b82600a8586648
Kirill <kirillilenkov974@gmail.com>
2023-07-03 18:10:09 +0300

    test_1

--- Base/test_1.py
+++ Base/test_1.py
@@ -23,8 +23,26 @@ class Point3D:
         self.y = y
         self.z = z
 
-pt = Point3D(1, 2, 3)
-pt.x = 6
-pt.y = 6
-pt.z = 6
-print(pt.x, pt.y, pt.z)
+    def __add__(self, other):
+        if not isinstance(other, (tuple, Point3D)):
+            raise ArithmeticError('Нужне кортеж с тремя числовыми параметрами или объект класса Point3D')
+
+        sc = other
+        if isinstance(other, Point3D):
+            sc = other.x, other.y, other.z
+
+        return self.__class__(self.x + sc[0], self.y + sc[1], self.z + sc[2])
+
+    def __radd__(self, other):
+        return self + other
+
+    def __str__(self):
+        return f'{self.x}, {self.y}, {self.z}'
+
+    def __iadd__(self, other):
+        return self + other
+
+pt_1 = Point3D(1, 1, 1)
+pt_2 = Point3D(2, 4, 8)
+pt_1 += pt_2
+print(pt_1)
