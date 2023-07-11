bf0608ab7474d6f80b73397c1b08d8f134483760
Kirill <kirillilenkov974@gmail.com>
2023-07-11 15:34:54 +0300

    add test_4, test_5

--- Base/test_4.py
+++ Base/test_4.py
@@ -1,9 +1,14 @@
 class Cup:
-    size = 3
+    __slots__ = ('bottom', 'wall')
 
+    def __init__(self, bottom, wall):
+        self.bottom = bottom
+        self.wall = wall
 class Big_Cup(Cup):
     pass
 
+m = Big_Cup(1, 3)
+m.L = 3
 
-m = Big_Cup()
-print(m.size)
+print(m.bottom, m.wall)
+print(m.__dict__)
